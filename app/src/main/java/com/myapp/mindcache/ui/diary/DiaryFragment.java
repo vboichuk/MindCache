1127ee3680a4c15dc0601db8bf7d4590c6823511
Valentyna Svistunova <svistunova.valentyna@gmail.com>
2025-06-24 14:56:06 +0300

    Initial commit

---
+++ app/src/main/java/com/myapp/mindcache/ui/diary/DiaryFragment.java
@@ -0,0 +1,37 @@
+package com.myapp.mindcache.ui.diary;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.myapp.mindcache.databinding.FragmentDiaryBinding;
+
+public class DiaryFragment extends Fragment {
+
+    private FragmentDiaryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DiaryViewModel diaryViewModel =
+                new ViewModelProvider(this).get(DiaryViewModel.class);
+
+        binding = FragmentDiaryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDiary;
+        diaryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/myapp/mindcache/ui/diary/DiaryViewModel.java
@@ -0,0 +1,19 @@
+package com.myapp.mindcache.ui.diary;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DiaryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DiaryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is diary fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
